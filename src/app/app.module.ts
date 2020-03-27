11fc51af333c1dbeb311bd2ad2e4729545353737
Kamil Chlebek <kamil.chlebek94@gmail.com>
2020-03-27 15:25:43 +0100

    Add @ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TranslateModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
